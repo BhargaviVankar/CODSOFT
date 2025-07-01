0f6f66dc22fdf918c991aaeab811364e3da30e37
BhargaviVankar <bhargavi31032005@gmail.com>
2025-07-01 22:33:46 +0530

    Added Calculator Project files
    Uploaded index.html, style.css, and script.js for Calculator functionality.

---
+++ CALCULATOR/script.js
@@ -0,0 +1,28 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+function toggleSign() {
+  if (display.value === "") return;
+
+  // Agar value already negative hai, to uska minus hata do
+  if (display.value.startsWith("-")) {
+    display.value = display.value.slice(1);
+  } else {
+    display.value = "-" + display.value;
+  }
+}
+
